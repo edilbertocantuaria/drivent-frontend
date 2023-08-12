f800ac2ff4f0879e8b8bf90c2071777a923e7d9c
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-08-12 00:43:47 -0300

    Feat: Login with github

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,4 +4,14 @@ export async function signIn(email, password) {
   const response = await api.post('/auth/sign-in', { email, password });
   return response.data;
 }
-//
+
+export async function githubLogin() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  console.log(code);
+  const response = api.post('/auth/sign-in-github', { code });
+  const { token } = response.data;
+  console.log(`token: ${token}`);
+  localStorage.setItem('token', token);
+}
+
